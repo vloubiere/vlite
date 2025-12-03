dee30047d9e9c189f42ccd09eb57512f3af54181
vloubiere <vincent.loubiere@imp.ac.at>
2025-12-03 10:39:27 +0100

    PROseq mask ncRNAs at the end

--- inst/dev.R
+++ inst/dev.R
@@ -208,6 +208,7 @@ if(F) {
   file.edit("inst/Rscript/DESeq2_analysis.R") # R subscript
 
   # PRO-Seq -----------------------
+  # Pipeline
   # Pipeline with ncRNA decoy (test)
   file.edit("R/create_rRNA_tRNA_bowtie_index_mm10.R")
   file.edit("R/proseqProcessing_ncRNAdecoy.R")
